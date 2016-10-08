72142efb5e486f271fc06e46d17eed81914073cc
Sherry <csherry.eddy@gmail.com>
2016-10-08 13:07:22 -0400

    add texting functionality through NexMo

--- weather_service.py
+++ weather_service.py
@@ -1,14 +1,30 @@
 import requests
-# import pprint
+import pprint
 
-# params = {
-# 	'q': 'Frederick,US',
-# 	'appid': '4ce082e5c4e81ec8283fde2f65796473',
-# 	'units' : 'imperial',
-# }
+# OpenWeatherMap API
+params = {
+	'q': 'Frederick,US',
+	'appid': '4ce082e5c4e81ec8283fde2f65796473',
+	'units' : 'imperial',
+}
 
-# response = requests.get(
-# 	'http://api.openweathermap.org/data/2.5/weather', params=params)
+response = requests.get(
+	'http://api.openweathermap.org/data/2.5/weather', params=params)
 
-# weather_data= response.json()
+weather_data = response.json()
 # pprint.pprint(weather_data)
+
+# pulls data to use in text message
+city = weather_data['name']
+temperature = weather_data['main']['temp']
+message = 'The weather in {} is {} degrees Fahrenheit.'.format(city, temperature)
+
+# NexMo API
+params = {
+    'api_key': '116d848a',
+    'api_secret': 'd1deb332d3f28425',
+    'from': '12132633755',
+    'to': '13049953889',
+    'text': message,
+}
+requests.get('https://rest.nexmo.com/sms/json', params=params)
