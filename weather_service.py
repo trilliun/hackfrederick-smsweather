bbaac7ec955f643bb8f176f61f4e772755493ac5
Sherry <csherry.eddy@gmail.com>
2016-10-08 14:47:24 -0400

    add multiple phone numbers

--- weather_service.py
+++ weather_service.py
@@ -1,5 +1,6 @@
 import requests
 import pprint
+import time
 
 # OpenWeatherMap API
 params = {
@@ -19,12 +20,18 @@ city = weather_data['name']
 temperature = weather_data['main']['temp']
 message = 'The weather in {} is {} degrees Fahrenheit.'.format(city, temperature)
 
+numbers = ['13049953889', '12402857580']
+
 # NexMo API
-params = {
-    'api_key': '116d848a',
-    'api_secret': 'd1deb332d3f28425',
-    'from': '12132633755',
-    'to': '13049953889',
-    'text': message,
-}
-requests.get('https://rest.nexmo.com/sms/json', params=params)
+for number in numbers: 
+	params = {
+	    'api_key': '116d848a',
+	    'api_secret': 'd1deb332d3f28425',
+	    'from': '12132633755',
+	    'to': number,
+	    'text': message,
+	}
+	requests.get('https://rest.nexmo.com/sms/json', params=params)
+	print(number)
+	time.sleep(1)
+
