42475632ba146df40d2ea7fe7a94360fe07fb5eb
Sherry <csherry.eddy@gmail.com>
2016-10-08 12:54:35 -0400

    add open weather service

---
+++ weather_service.py
@@ -0,0 +1,14 @@
+import requests
+# import pprint
+
+# params = {
+# 	'q': 'Frederick,US',
+# 	'appid': '4ce082e5c4e81ec8283fde2f65796473',
+# 	'units' : 'imperial',
+# }
+
+# response = requests.get(
+# 	'http://api.openweathermap.org/data/2.5/weather', params=params)
+
+# weather_data= response.json()
+# pprint.pprint(weather_data)
